b9adabccb00e723915a72845e14da2d91b6621d9
princemaurya71 <mr.prince3650@gmail.com>
2026-07-17 17:28:18 +0530

    update

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 const button = document.getElementById("toggleBtn");
+const img = document.getElementById("img1");
+const v1 = document.getElementById("video-background");
+const heading = document.getElementById("heading");
 let isDarkMode = false;
 
 button.addEventListener("click", async () => {
   isDarkMode = !isDarkMode;
 
   button.textContent = isDarkMode ? "Enable Light Mode" : "Enable Dark Mode";
-
+  heading.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
   let tabs = await chrome.tabs.query({
     active: true,
     currentWindow: true,
@@ -14,5 +17,13 @@ button.addEventListener("click", async () => {
   chrome.tabs.sendMessage(tabs[0].id, {
     action: isDarkMode ? "darkMode" : "lightMode",
   });
-//   button.textContent = "Successfully Start...";
+  if(isDarkMode){
+    img.src = "icons/night-mode.png";
+    v1.src = "video/v1.mp4";
+  }else{
+    img.src = "icons/day-mode.png";
+    v1.src = "video/v2.mp4";
+  }
+  
 });
+
